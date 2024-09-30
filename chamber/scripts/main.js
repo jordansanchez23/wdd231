020f4984259e9b6e492b40a321eb4d8c7051da7d
jordansanchez23 <joivisvis@hotmail.com>
2024-09-29 21:14:24 -0700

    hamburger menu done

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,11 +1,19 @@
 /*********Current Year **********/
-
-
 const currentYear = new Date().getFullYear();
 
 const currentYearElement = document.getElementById("currentYear");
 currentYearElement.textContent = currentYear;
 
+/************Last Modified************/
 const lastModified = document.lastModified;
 const lastModifiedElement =document.getElementById("lastModified");
 lastModifiedElement.textContent = `${lastModified}`;
+
+/**********Menu**************/
+const mainnav = document.querySelector(".navigation");
+const hambutton = document.querySelector("#menu");
+
+hambutton.addEventListener("click", () => {
+    mainnav.classList.toggle("show");
+    hambutton.classList.toggle("show");
+});
