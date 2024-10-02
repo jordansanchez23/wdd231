2b22a8b8cfcb8797a7bea2f13d705a21489f6d24
jordansanchez23 <joivisvis@hotmail.com>
2024-10-02 12:25:52 -0700

    ES modules

---
+++ es-modules/modules.js
@@ -0,0 +1,13 @@
+//module.js
+import aCourse from "./Course.mjs";
+
+aCourse.init();
+  document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum);
+  });
+  document.querySelector("#dropStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.changeEnrollment(sectionNum, false);
+  });
+  
